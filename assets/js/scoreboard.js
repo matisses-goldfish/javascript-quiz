1337aaefa590561ba6fbde77d0f6d489c7c4b1ea
matisses goldfish <honoregn@gmail.com>
2022-03-19 01:02:27 -0500

    updating file structure

--- assets/js/scoreboard.js
+++ assets/js/scoreboard.js
@@ -1,108 +1,58 @@
-// code sourced from Mengmei Tu: https://github.com/mmeii/code-quiz/blob/main/Assets/script.js
+// Scoreboard
+var scoreBoard = document.querySelector("#scoreBoard");
+var highScores = document.querySelector("#highScores");
 
-var time = document.getElementById("time");
-var remainingTime = document.getElementById("remainingTime");
-var endTime = document.getElementById("endTime");
+// create Iniatials/Name
+var nameForm = document.querySelector("#nameForm");
+var nameInput = document.querySelector("#nameInput");
 
-var playAgainBtn = document.getElementById("playAgainBtn");
+var nameBtn = document.querySelector("#nameBtn");
+nameBtn.addEventListener("click", storeAndAppend);
 
+var statName = document.querySelector("#statName");
 
-var nameBtn = document.getElementById("nameBtn");
-var statName = document.getElementById("statName");
-var highScores = document.getElementById("highScores");
-var nameForm = document.getElementById("nameForm");
-var clearAllScores = document.getElementById("clearAllScores");
+// Clear scores / Play again
+var clearAllScores = document.querySelector("#clearAllScores");
+var endGame = document.querySelector("#endGame");
+var playAgainBtn = document.querySelector("playAgainBtn");
 
-var questContainer = document.getElementById("questContainer");
-
-var endGame = document.getElementById("endGame");
-var nameInput = document.getElementById("nameInput");
-var scoreBoard = document.getElementById("scoreBoard");
-
-
-// store high score:
-function storeScores (event){
-    event.preventDefault();
-
-        if (nameInput.value === "") {
-        alert("please leave your name");
-        return;
-    } 
-
+function pageformat () {
     nameForm.style.display = "none";
     highScores.style.display = "block";   
-    endGame.style.display = "block";
-    questContainer.style.display = "none";
-    time.style.display = "none";
-    endTime.style.display = "block";
     clearAllScores.style.display = "block";
+}
 
-    var savedScore = localStorage.getItem("high scores");
-    var scoresArray;
+function storeAndAppend () {
+    renderScore();
+    pageformat ();
 
-    if (savedScore === null) {
-        scoresArray = [];
-    } else {
-        scoresArray = JSON.parse(savedScore)
-    }
-
-    var clientStats = {
-        name: nameInput.value,
-        score: scoreBoard.textContent
-    };
-
-    console.log(clientStats);
-    scoresArray.push(clientStats);
-
-    var scoresArrayString = JSON.stringify(scoresArray);
-    window.localStorage.setItem("high scores", scoresArrayString);
+    scoreBoard.textContent = playerPoints;
     
     // show current highscores
-    showHighScores();
+    displayScoreBoard();
+    // take playerPoints and append with name input into json local storage and score board
 }
 
-// Display Scores
+// display scores 
+// might not need this
 var i = 0;
-function showHighScores() {
+function displayScoreBoard() {
 
-    time.style.display = "none";
-    questContainer.style.display = "none";
-    endTime.style.display = "block";
-    endGame.style.display = "block";
-    remainingTime.style.display = "none";
-    highScores.style.display = "block";
+    renderScore();
 
-    var statsBoard = localStorage.getItem("high scores");
-
-    // check if there is any in local storage
-    if (statsBoard === null) {
-        return;
-    }
-    console.log(statsBoard);
-
-    var savedStats = JSON.parse(statsBoard);
-
-    for (; i < savedStats.length; i++) {
-        var newStat = document.createElement("div");
-        newStat.setAttribute('class', 'card text-left');
-        newStat.setAttribute('id', 'statBoard');
-        newStat.innerHTML =  "Name: " + savedStats[i].name + " Stats: " + savedStats[i].score ;
-        highScores.appendChild(newStat);
-    }
 }
-// Display Stats
-nameBtn.addEventListener("click", function(event){ 
-    storeScores(event); 
-});
 
 // clear scores
-clearAllScores.addEventListener("click", function(){
+clearAllScores.addEventListener("click", clearStats);
+
+function clearStats () {
     window.localStorage.clear("statBoard");
     highScores.innerHTML = "Cleared Stats Board!";
-    clearAllScores.style.display = "none";
-});
+    clearAllScores.style.display = "none"; 
+}
 
-// Play Again
+
+// Play again
 playAgainBtn.addEventListener("click", function(event){ 
     window.location.reload();
 });
