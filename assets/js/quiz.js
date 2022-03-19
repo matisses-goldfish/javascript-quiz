7d1a8f85656c6bd6e47018c71efd72dcb4a21e3c
matisses goldfish <honoregn@gmail.com>
2022-03-18 21:41:17 -0500

    corrected file structure

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,116 +1,175 @@
-// code sourced from Mengmei Tu: https://github.com/mmeii/code-quiz/blob/main/Assets/script.js
+// sourced timer: https://stackoverflow.com/questions/44314897/javascript-timer-for-a-quiz 
+// iniatial reference: https://github.com/mmeii/code-quiz/blob/main/Assets/script.js
 
-// Iniatial Landing Page for Quiz
-var startQuizDiv = document.getElementById("startQuizDiv");
+// Welocome page
+var welcomePage = document.querySelector("#welcomePage");
 
-// time vars
-var time = document.getElementById("time");
-var remainingTime = document.getElementById("remainingTime");
-var endTime = document.getElementById("endTime");
+// Begin quiz
+var beginQuizBtn = document.querySelector("#beginQuizBtn");
+beginQuizBtn.addEventListener("click", beginQuiz);
 
-// button vars
-var startBtn = document.getElementById("startBtn");
+// Questions
+var renderQuestions = document.querySelector("#renderQuestions");
+var singleQuestion = document.querySelector("#singleQuestion");
+var userAnswers = document.querySelector("#userAnswers");
 
-// question shuffle vars
-var questContainer = document.getElementById("questContainer");
-var questHeader = document.getElementById("questHeader");
-var buttonA = document.getElementById("btnA");
-var buttonB = document.getElementById("btnB");
-var buttonC = document.getElementById("btnC");
-var buttonD = document.getElementById("btnD");
-var answerCheck = document.getElementById("answerCheck");
+// Iniatialize Answer buttons
+var answerClickA = document.querySelector("#answerClickA");
+var answerClickB = document.querySelector("#answerClickB");
+var answerClickC = document.querySelector("#answerClickC");
+var answerClickD = document.querySelector("#answerClickD");
 
-// end/score vars
-var endGame = document.getElementById("endGame");
-var nameInput = document.getElementById("nameInput");
-var scoreBoard = document.getElementById("scoreBoard");
+// Check answer 
+// var answerClick = document.querySelector(".answerClick");
+// answerClick.addEventListener("click", alertCorrect);
 
-// declaring basic vars
-var correction = 0;
-var quizNum = 0;
+// answerClickA.addEventListener("click", displayA);
+// answerClickB.addEventListener("click", displayB);
+// answerClickC.addEventListener("click", displayC);
+// answerClickD.addEventListener("click", displayD);
 
-// time and quiz shuffle
-startBtn.addEventListener("click", startQuest);
-buttonA.addEventListener("click", number0);
-buttonB.addEventListener("click", number1);
-buttonC.addEventListener("click", number2);
-buttonD.addEventListener("click", number3);
+// Next Question
+nextQuestionBtn = document.querySelector("#nextQuestionBtn")
+nextQuestionBtn.addEventListener("click", nextQuestion);
 
-var playerClock = 151;
+var correction = document.querySelector("#correction");
 
-function startQuest() {
-    quizNum = 0;
-    playerClock = 150;
-    remainingTime.textContent = playerClock;
-    nameInput.textContent = "";
+// time 
+var time = document.querySelector("#time");
+var remainingTime = document.querySelector("#remainingTime");
+var endTime = document.querySelector("#endTime");
 
-    startQuizDiv.style.display = "none";
-    questContainer.style.display = "block";
+// Extra additions (time, scoreboard, and shuffle)
+var quizTimer = 101;
+var playerPoints = 500;
+var questionNumber = 0;
+
+
+
+// shuffle through questions 
+
+// start quiz
+function beginQuiz () {
+    questionNumber = 0;
+
+    // render elements 
+    welcomePage.style.display = "none";
+    renderQuestions.style.display = "block";
     time.style.display = "block";
     endTime.style.display = "none";
 
-    var startTimer = setInterval(function() {
-        playerClock--;
-        remainingTime.textContent = playerClock;
-        if(playerClock <= 0) {
-            clearInterval(startTimer);
-            if (quizNum < question.length - 1) {
-                stopGame();
-            }
+    // call function renderQuestions
+    renderQuestionIndex();
+
+    // call function renderChoices
+    renderChoices();
+
+    // call function beginTime
+    beginTime();
+}
+
+// quiz timer
+// timer based on: https://stackoverflow.com/questions/44314897/javascript-timer-for-a-quiz 
+function beginTime () {
+    quizTimer = 100;
+
+    var interval = setInterval(function(){
+        quizTimer--;
+        remainingTime.textContent = quizTimer;
+
+        // quizTimer === 0 wasnt computing???
+        if (quizTimer <= 0){
+          clearInterval(interval);
+          gameOver();
+            
         }
-    },1000);
-
-    displayQuest();
-};
-
-// question shuffle 
-function displayQuest() {
-    following();
-}
-
-function following() {
-    questHeader.textContent = totalQuest[quizNum].question;
-    buttonA.textContent = totalQuest[quizNum].choices[0];
-    buttonB.textContent = totalQuest[quizNum].choices[1];
-    buttonC.textContent = totalQuest[quizNum].choices[2];
-    buttonD.textContent = totalQuest[quizNum].choices[3];
-}
-
-// check answer 
-function displayAnswer(correctAnswer) {
-    answerCheck.style.display = "block";
-
-    if (totalQuest[quizNum].correctAnswer === totalQuest[quizNum].choices[correctAnswer]) {
-        correction++;
-        answerCheck.textContent = "Correct!";
-    } else {
-        playerClock -= 10;
-        remainingTime.textContent = playerClock;
-        answerCheck.textContent = "Wrong! The correct answer is: " + totalQuest[quizNum].correctAnswer;
+      }, 1000);
     }
 
-    quizNum++;
-    if (quizNum < totalQuest.length) {
-        following();
+    var ulCreate = document.createElement("ul");
+
+// render questions 
+function renderQuestionIndex () {
+    ulCreate.innerHTML = "";
+
+        for (var i = 0; i < totalQuest.length; i++) {
+        // renders question
+        var questionTitle = totalQuest[questionNumber].question;
+        singleQuestion.textContent = questionTitle;
+        }
+}
+
+var ulCreate = document.createElement("ul");
+var liCreate = document.createElement("li");
+
+function renderChoices () {
+    for (var i = 0; i < totalQuest.length; i++) {
+        // renders choices
+        var answerChoices = totalQuest[questionNumber].choices;
+
+    }
+    answerChoices.forEach(function (newItem) {
+        var newButton = document.createElement("button");
+        newButton.textContent = newItem;
+        newButton.setAttribute('class', 'btn btn-primary answerClick');
+
+        userAnswers.appendChild(ulCreate);
+        ulCreate.appendChild(newButton);
+        newButton.addEventListener("click", (alertCorrect));
+    })
+}
+
+// render choices 
+// function renderChoices () {
+//     answerClickA.textContent = totalQuest[questionNumber].choices[0];
+//     answerClickB.textContent = totalQuest[questionNumber].choices[1];
+//     answerClickC.textContent = totalQuest[questionNumber].choices[2];
+//     answerClickD.textContent = totalQuest[questionNumber].choices[3];
+// }
+
+
+// display correct
+function alertCorrect(correctAnswer) {
+    if (totalQuest[questionNumber].correctAnswer === totalQuest[questionNumber].choices[correctAnswer]) {
+        playerPoints +=100;
+        correction.style.display= "block"
+        correction.textContent = "Correct! Nicely Done!";
     } else {
-        stopGame();
+        quizTimer -= 10;
+        remainingTime.textContent = quizTimer;
+        correction.style.display= "block";
+        correction.textContent = "Wrong! The correct answer is: " + totalQuest[questionNumber].correctAnswer;
     }
 }
 
-function displayA() { displayAnswer(0); }
+function displayA() { alertCorrect(0); }
 
-function displayB() { displayAnswer(1); }
+function displayB() { alertCorrect(1); }
 
-function displayC() { displayAnswer(2); }
+function displayC() { alertCorrect(2); }
 
-function displayD() { displayAnswer(3); }
+function displayD() { alertCorrect(3); }
 
-// game over
-function stopGame() {
+
+// formated after displayAnswer from : https://github.com/mmeii/code-quiz/blob/main/Assets/script.js
+function nextQuestion() {
+    questionNumber++;
+    if (questionNumber < totalQuest.length) {
+        renderQuestionIndex();
+        renderChoices();
+        correction.style.display= "none"
+    } else {
+        gameOver();
+    }
+}
+
+
+function gameOver () {
     endGame.style.display = "block";
-    questContainer.style.display = "none";
+    renderQuestions.style.display = "none";
     time.style.display = "none";
-    endTime.style.display = "block";
+    remainingTime.style.display = "none";
+    endTime.style.display = "block";    
 
-    scoreBoard.textContent = correction;
+    scoreBoard.textContent = playerPoints;
 }
